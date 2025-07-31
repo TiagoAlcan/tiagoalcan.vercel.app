0d7f53e94ad2e9172eede53c87937f0129c84dbf
TiagoAlcan <tiago.gomesalcan@gmail.com>
2025-07-31 15:43:32 -0300

    feat: add fonts and vars.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import "@fontsource-variable/geist-mono"
+import "@fontsource-variable/outfit"
+import "@fontsource/roboto"
+import "@fontsource/jetbrains-mono"
+import "@fontsource/ubuntu"
+import "@fontsource/ibm-plex-mono"
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
